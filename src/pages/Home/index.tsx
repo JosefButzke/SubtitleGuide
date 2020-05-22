3f5d9b67c62e6f2d27329e3956925b3d512c59b2
JosefButzke <josef-stein@hotmail.com>
2020-05-21 23:02:57 -0300

    add parse srt

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
-import { Container, Title } from './styles';
+import * as Subtitle from 'subtitle';
+import { Container, Title, SubTitleContainer, SubTitleLine } from './styles';
 
 const Home: React.FC = () => {
+  const [subtitle, setSubtitle] = useState([]);
+
+  useEffect(() => {
+    const loadSubtitle = async () => {
+      const text = `3
+00:00:16,149 --> 00:00:18,618
+（YOU）テラスハウスは
+見ず知らずの男女６人が`;
+
+      const textParsed = Subtitle.parse(text);
+      console.log(textParsed);
+      setSubtitle(textParsed);
+    };
+    loadSubtitle();
+  }, []);
   return (
     <Container>
-      <Title>Home</Title>
+      <Title>Subtitle</Title>
+      <SubTitleContainer>
+        {subtitle.map((sub) => (
+          <SubTitleLine key={String(sub.start)}>
+            {sub.text.replace(/(\r\n|\n|\r)/gm, '')}
+          </SubTitleLine>
+        ))}
+      </SubTitleContainer>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -3,7 +3,19 @@ import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 export const Container = styled.View`
   flex: 1;
-  padding-top: ${getStatusBarHeight(true)};
+  padding-top: ${getStatusBarHeight(true)}px;
+  align-items: center;
 `;
 
-export const Title = styled.Text``;
+export const Title = styled.Text`
+  font-size: 32px;
+`;
+
+export const SubTitleContainer = styled.View`
+  flex: 1;
+  width: 100%;
+  padding: 0 20px;
+  margin-top: 30px;
+`;
+
+export const SubTitleLine = styled.Text``;
